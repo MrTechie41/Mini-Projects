881194816e0da3861afe3cb32de5e766164e485b
Dinesh Zatke <zatakedinesh007@gmail.com>
2023-07-24 12:09:19 +0530

    Game over design

--- Quiz Game/quiz_game.py
+++ Quiz Game/quiz_game.py
@@ -10,38 +10,41 @@ score = 0
 
 answer = input("What does CPU stand for? ")
 if answer.lower() == "central processing unit":
-    print("Correct!")
+    print("-- Correct! --")
     score += 1
 else:
-    print("Incorrect!")
+    print("-- Incorrect! --")
 
 answer = input("What does GPU stand for? ")
 if answer.lower() == "graphics processing unit":
-    print("Correct!")
+    print("-- Correct! --")
     score += 1
 else:
-    print("Incorrect!")
+    print("-- Incorrect! --")
 
 answer = input("What does RAM stands for? ")
 if answer.lower() == "random access memory":
-    print("Correct!")
+    print("-- Correct! --")
     score += 1
 else:
-    print("Incorrect!")
+    print("-- Incorrect! --")
 
 answer = input("What does PSU stands for? ")
 if answer.lower() == "power supply":
-    print("Correct!")
+    print("-- Correct! --")
     score += 1
 else:
-    print("Incorrect!")
+    print("-- Incorrect! --")
 
 answer = input("What does TCP stands for? ")
 if answer.lower() == "transmission control protocol":
-    print("Correct!")
+    print("-- Correct! --")
     score += 1
 else:
-    print("Incorrect!")
+    print("-- Incorrect! --")
 
 print("You got " + str(score) + " questions correct!")
+
 print("You got " + str((score / 5) * 100) + " %.")
+
+print("----- Game Over -----")
