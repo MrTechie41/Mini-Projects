43292c798bdaa7a1bb6611978b39029e682b0bb9
Dinesh Zatke <zatakedinesh007@gmail.com>
2023-07-24 12:05:32 +0530

    Some modifications

---
+++ Quiz Game/quiz_game.py
@@ -0,0 +1,46 @@
+print("----- Welcome to the quiz! -----")
+
+playing = input("Do you want to play (yes/no)? ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("Let's play then :)")
+score = 0
+
+answer = input("What does CPU stand for? ")
+if answer.lower() == "central processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does GPU stand for? ")
+if answer.lower() == "graphics processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does RAM stands for? ")
+if answer.lower() == "random access memory":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does PSU stands for? ")
+if answer.lower() == "power supply":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does TCP stands for? ")
+if answer.lower() == "transmission control protocol":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+print("You got " + str(score) + " questions correct!")
